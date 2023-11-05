cf5faaad49e451cb1597b128e7b99d4ebafe36ab
BachNee <shiina3101@gmail.com>
2023-11-05 18:07:52 +0700

    Login and Signup test

---
+++ js/my js/login signup.js
@@ -0,0 +1,7 @@
+function signup() {
+    var username = document.getElementById("signupUsername").value
+    var password = document.getElementById("gisnupPassword").value
+    var checkPassword = document.getElementById('signupCheckPassword').value
+    var email = document.getElementById('signupEmail').value
+    
+}
